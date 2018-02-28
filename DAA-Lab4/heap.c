128c508faa993f42a4ed480ca4726789a96aba1d
Niharika Gali <33419599+niharikagali@users.noreply.github.com>
2018-02-28 14:25:38 +0530

    Add files via upload

---
+++ DAA-Lab4/heap.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isMaxHeap(int *arr, int i, int n);
+int isMinHeap(int *arr, int i, int n);
+
+float timeEval = 0;
+
+int main()
+{
+	int n,*arr,i;
+
+	scanf("%d",&n); //scan number of elements in the input
+
+	arr = (int *)malloc(n * sizeof(int));
+
+	for (i = 0; i < n; ++i)
+	{
+		scanf("%d",&arr[i]);
+		timeEval += 3;
+	}
+
+	if(isMaxHeap(arr,0,n))
+	{
+		printf("The given sequence of numbers form a max heap.\n");
+	}
+	else if(isMinHeap(arr,0,n))
+	{
+		printf("The given sequence of numbers form a min heap.\n");
+	}
+	else
+	{
+		printf("The given sequence of numbers doesn't form a heap.\n");
+	}
+
+	timeEval = timeEval/(float)100000;
+  	printf("The time taken for computation is %f\n",timeEval);
+
+	return 0;
+}
+
+int isMaxHeap(int *arr, int i, int n)
+{
+	if(i > (n-2)/2)
+	{
+		timeEval += 1;
+		return 1;		//if i is a leaf node, return true.
+	}
+	else
+	{
+		if(arr[i] >= arr[2*i + 1] && arr[i] >= arr[2*i + 2] && isMaxHeap(arr,2*i + 1,n) && isMaxHeap(arr,2*i + 2, n))
+		{
+			timeEval += 4;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int isMinHeap(int *arr, int i, int n)
+{
+	if(i > (n-2)/2)
+	{
+		timeEval += 1;
+		return 1;		//if i is a leaf node, return true.
+	}
+	else
+	{
+		if(arr[i] <= arr[2*i + 1] && arr[i] <= arr[2*i + 2] && isMinHeap(arr,2*i + 1,n) && isMinHeap(arr,2*i + 2, n))
+		{
+			timeEval += 4;
+			return 1;
+		}
+	}
+
+	return 0;
+}
